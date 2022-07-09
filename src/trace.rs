a210b7ad46fa62c9d981d53a1a81fd54c107adb0
DevAles <devalesoficial@gmail.com>
2022-07-09 11:37:12 -0300

    Implements tests for trace

--- src/trace.rs
+++ src/trace.rs
@@ -139,3 +139,74 @@ pub fn trace(cpu: &CPU) -> String {
     )
     .to_ascii_uppercase()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::components::bus::BUS;
+    use crate::components::cartridges::test::test_rom;
+
+    #[test]
+
+    fn test_format_trace() {
+        let mut bus = BUS::new(test_rom());
+        bus.memory_write(100, 0xa2);
+        bus.memory_write(101, 0x01);
+        bus.memory_write(102, 0xca);
+        bus.memory_write(103, 0x88);
+        bus.memory_write(104, 0x00);
+
+        let mut cpu = CPU::new(bus);
+        cpu.register_pc = 0x64;
+        cpu.register_a = 1;
+        cpu.register_x = 2;
+        cpu.register_y = 3;
+
+        let mut result: Vec<String> = vec![];
+        cpu.run_with_callback(|cpu| {
+            result.push(trace(cpu));
+        });
+
+        assert_eq!(
+            "0064  A2 01     LDX #$01                        A:01 X:02 Y:03 P:24 SP:FD",
+            result[0]
+        );
+        assert_eq!(
+            "0066  CA        DEX                             A:01 X:01 Y:03 P:24 SP:FD",
+            result[1]
+        );
+        assert_eq!(
+            "0067  88        DEY                             A:01 X:00 Y:03 P:26 SP:FD",
+            result[2]
+        );
+    }
+
+    #[test]
+    fn test_format_memory_acess() {
+        let mut bus = BUS::new(test_rom());
+        // ORA ($33), Y
+        bus.memory_write(100, 0x11);
+        bus.memory_write(101, 0x33);
+
+        //data
+        bus.memory_write(0x33, 00);
+        bus.memory_write(0x34, 04);
+
+        //target cell
+        bus.memory_write(0x400, 0xAA);
+
+        let mut cpu = CPU::new(bus);
+        cpu.register_pc = 0x64;
+        cpu.register_y = 0;
+
+        let mut result: Vec<String> = vec![];
+        cpu.run_with_callback(|cpu| {
+            result.push(trace(cpu));
+        });
+
+        assert_eq!(
+            "0064  11 33     ORA ($33),Y = 0400 @ 0400 = AA  A:00 X:00 Y:00 P:24 SP:FD",
+            result[0]
+        );
+    }
+}
