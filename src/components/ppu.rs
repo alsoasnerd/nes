f5be3e18b1c3d86234d1a0bb9f03a5a487ff662d
DevAles <devalesoficial@gmail.com>
2022-07-14 19:42:53 -0300

    Implement ControlRegister

--- src/components/ppu.rs
+++ src/components/ppu.rs
@@ -1,9 +1,59 @@
 use super::cartridges::Mirroring;
+use bitflags::bitflags;
+
+bitflags! {
+
+   // 7  bit  0
+   // ---- ----
+   // VPHB SINN
+   // |||| ||||
+   // |||| ||++- Base nametable address
+   // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+   // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+   // |||| |     (0: add 1, going across; 1: add 32, going down)
+   // |||| +---- Sprite pattern table address for 8x8 sprites
+   // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+   // |||+------ Background pattern table address (0: $0000; 1: $1000)
+   // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+   // |+-------- PPU master/slave select
+   // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+   // +--------- Generate an NMI at the start of the
+   //            vertical blanking interval (0: off; 1: on)
+
+   pub struct ControlRegister: u8 {
+       const NAMETABLE1                = 0b00000001;
+       const NAMETABLE2                = 0b00000010;
+       const VRAM_ADD_INCREMENT        = 0b00000100;
+       const SPRITE_PATTERN_ADDRESS    = 0b00001000;
+       const BACKROUND_PATTERN_ADDRESS = 0b00010000;
+       const SPRITE_SIZE               = 0b00100000;
+       const MASTER_SLAVE_SELECT       = 0b01000000;
+       const GENERATE_NMI              = 0b10000000;
+   }
+}
+
+impl ControlRegister {
+    pub fn new() -> Self {
+        ControlRegister::from_bits_truncate(0b00000000)
+    }
+
+    pub fn vram_address_increment(&self) -> u8 {
+        if self.contains(ControlRegister::VRAM_ADD_INCREMENT) {
+            32
+        } else {
+            1
+        }
+    }
+
+    pub fn update(&mut self, value: u8) {
+        self.bits = value;
+    }
+}
 
 pub struct AddressRegister {
     low: u8,
     high: u8,
-    high_pointer: bool
+    high_pointer: bool,
 }
 
 impl AddressRegister {
@@ -11,7 +61,7 @@ impl AddressRegister {
         Self {
             low: 0,
             high: 0,
-            high_pointer: true
+            high_pointer: true,
         }
     }
 
@@ -65,7 +115,7 @@ pub struct PPU {
     pub vram: [u8; 2048],
     pub oam_data: [u8; 256],
     pub mirroring: Mirroring,
-    address: AddressRegister
+    address: AddressRegister,
 }
 
 impl PPU {
@@ -76,7 +126,7 @@ impl PPU {
             vram: [0; 2048],
             oam_data: [0; 64 * 4],
             mirroring,
-            address: AddressRegister::new()
+            address: AddressRegister::new(),
         }
     }
 
