6fdcfa317bfd640e8ef90e41ed880fbb32882db8
DevAles <devalesoficial@gmail.com>
2022-08-22 20:23:12 -0300

    Implement keymap in Joypad

--- src/components/joypads.rs
+++ src/components/joypads.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+use sdl2::keyboard::Keycode;
+
 bitflags! {
     struct JoypadButton: u8 {
         const RIGHT             = 0b1000_0000;
@@ -14,15 +18,29 @@ bitflags! {
 pub struct Joypad {
     strobe_mode: bool,
     button_index: u8,
-    button_status: JoypadButton
+    button_status: JoypadButton,
+    keymap: HashMap<Keycode, JoypadButton>
 }
+
+
 
 impl Joypad {
     pub fn new() -> Self {
+        let mut keymap = HashMap::new();
+        keymap.insert(Keycode::W, JoypadButton::UP);
+        keymap.insert(Keycode::A, JoypadButton::LEFT);
+        keymap.insert(Keycode::S, JoypadButton::DOWN);
+        keymap.insert(Keycode::D, JoypadButton::RIGHT);
+        keymap.insert(Keycode::Space, JoypadButton::BUTTON_A);
+        keymap.insert(Keycode::E, JoypadButton::BUTTON_B);
+        keymap.insert(Keycode::Return, JoypadButton::START);
+        keymap.insert(Keycode::Tab, JoypadButton::SELECT);
+
         Joypad {
             strobe_mode: false,
             button_index: 0,
             button_status: JoypadButton::from_bits_truncate(0b0000_0000),
+            keymap
         }
     }
 
